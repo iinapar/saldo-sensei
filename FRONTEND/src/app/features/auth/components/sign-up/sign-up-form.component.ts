5dbe606c5dea9da4c6be8d19b828988c3fa9179e
Iina Parikainen <ab7383@student.jamk.fi>
2025-03-05 10:29:58 +0200

    [frontend] feat: Add form-control for sign-up form

--- FRONTEND/src/app/features/auth/components/sign-up/sign-up-form.component.ts
+++ FRONTEND/src/app/features/auth/components/sign-up/sign-up-form.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   standalone: true,
+  imports: [ReactiveFormsModule],
   selector: 'sign-up-form',
   templateUrl: './sign-up-form.component.html',
   styleUrl: './sign-up-form.component.css',
 })
-export class SignUpFormComponent {}
+export class SignUpFormComponent {
+  signUpForm = new FormGroup({
+    firstname: new FormControl('', [Validators.required]),
+    lastname: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  signUp() {
+    console.log(this.signUpForm.value);
+  }
+}
