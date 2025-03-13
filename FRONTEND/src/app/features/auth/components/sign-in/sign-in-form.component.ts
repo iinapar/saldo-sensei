f0ab5537ca94b5a90478d7a1ff7eed4dce7bf082
Iina Parikainen <ab7383@student.jamk.fi>
2025-03-13 09:13:51 +0200

    [frontend] feat: Add form-control for sign-in form

--- FRONTEND/src/app/features/auth/components/sign-in/sign-in-form.component.ts
+++ FRONTEND/src/app/features/auth/components/sign-in/sign-in-form.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   standalone: true,
+  imports: [ReactiveFormsModule],
   selector: 'sign-in-form',
   templateUrl: './sign-in-form.component.html',
   styleUrl: './sign-in-form.component.css',
 })
-export class SignInFormComponent {}
+export class SignInFormComponent {
+  errorMessage: string | null = null;
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
+
+  constructor() {}
+
+  login() {
+    console.log('Login');
+  }
+}
